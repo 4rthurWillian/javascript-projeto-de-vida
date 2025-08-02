3f8efcabdc779e565647466ff950d05276dca3f9
4rthurWillian <pohlod.arthur@escola.pr.gov.br>
2025-08-02 23:41:14 +0000

    .

--- main.js
+++ main.js
@@ -14,10 +14,6 @@ for(let i = 0; i< botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
-
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
 const tempoObjetivo2 = new Date("2025-12-05T00:00");
 const tempoObjetivo3 = new Date("2025-12-20T00:00");
 const tempoObjetivo4 = new Date("2026-02-05T00:00");
@@ -45,7 +41,7 @@ function calculaTempo(tempoObjetivo){
 
 function atualizaCronometro(){
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("doras0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
     document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
     for (let i = 0; i < contadores.length; i++){
